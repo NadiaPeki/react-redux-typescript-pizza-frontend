11bef1e6a04350a7d640aaeaf1cb40aced717b00
Nadzeya Pekarskaya <napekarskaya@gmail.com>
2024-04-01 13:52:10 +0200

    created PizzaBlock, Categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useState } from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  console.log(activeIndex);
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => {
+          return (
+            <li
+              onClick={() => setActiveIndex(i)}
+              className={activeIndex === i ? 'active' : ' '}
+              key={i}>
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
